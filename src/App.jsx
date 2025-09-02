180a172441837daf7b2ca8df49caad1fc1115218
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-09-02 11:49:00 +0300

    feat: integrate PreventPullToRefresh component to enhance mobile experience

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './windows2000.css'
 import { DesktopRoot } from './components/desktop/DesktopRoot/DesktopRoot.jsx'
 import Win2kLogin from './components/win2k-login/Win2kLogin.jsx'
+import PreventPullToRefresh from './utils/PreventPullMobile/PreventPullToRefresh.jsx'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -10,12 +11,15 @@ function App() {
     setLoggedIn(false)
   }
 
-  if (!loggedIn) {
-    return <Win2kLogin onLogin={() => setLoggedIn(true)} />
-  }
-
-  return <DesktopRoot onShutdown={handleShutdown} />
+  return (
+    <PreventPullToRefresh>
+      {!loggedIn ? (
+        <Win2kLogin onLogin={() => setLoggedIn(true)} />
+      ) : (
+        <DesktopRoot onShutdown={handleShutdown} />
+      )}
+    </PreventPullToRefresh>
+  )
 }
 
 export default App
-
