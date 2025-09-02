17d78aa4b12ed124321c187eec97efa44e3f8484
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-09-02 12:32:55 +0300

    refactor: remove PreventPullToRefresh component and adjust App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import './windows2000.css'
 import { DesktopRoot } from './components/desktop/DesktopRoot/DesktopRoot.jsx'
 import Win2kLogin from './components/win2k-login/Win2kLogin.jsx'
-import PreventPullToRefresh from './utils/PreventPullMobile/PreventPullToRefresh.jsx'
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false)
@@ -11,16 +10,12 @@ function App() {
     setLoggedIn(false)
   }
 
-  return (
-    <PreventPullToRefresh>
-      {!loggedIn ? (
-        <Win2kLogin onLogin={() => setLoggedIn(true)} />
-      ) : (
-        <DesktopRoot onShutdown={handleShutdown} />
-        
-      )}
-    </PreventPullToRefresh>
-  )
+  if (!loggedIn) {
+    return <Win2kLogin onLogin={() => setLoggedIn(true)} />
+  }
+
+  return <DesktopRoot onShutdown={handleShutdown} />
 }
 
 export default App
+
