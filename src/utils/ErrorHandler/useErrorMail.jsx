be732ffbec6d9d971709ffb11de33fc0d4c929d1
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2025-09-07 10:37:17 +0300

    fix: update warning icon import in useErrorMail hook for consistency

--- src/utils/ErrorHandler/useErrorMail.jsx
+++ src/utils/ErrorHandler/useErrorMail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './useErrorMail.css'
+import warningIcon from '../../assets/win7/icons/warning.ico'
 
 /**
  * useErrorMail - Custom hook for rendering error tooltips for form fields
@@ -16,7 +17,7 @@ export function useErrorMail() {
       <div className="input-error-tooltip-wrapper">
         {errors[field] && (
           <span className="input-error-tooltip">
-            <img src="/src/assets/win7/icons/warning.ico" alt="warning" />
+            <img src={warningIcon} alt="warning" />
             {errors[field]}
           </span>
         )}
